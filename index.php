fb2f5f5d90daf78ff12fc2780513f224a0fb101c
Adolfo Rodriguez <adolfo@merchlar.com>
2014-05-21 16:57:11 -0400

    Added text for testing

--- index.php
+++ index.php
@@ -1,1 +1,41 @@
-<html><i>Oh yeah!</i> - Kool Aid Man</html>
+<html><i>Oh yeah!</i> &ndash; Kool Aid Man
+<br/>
+<br/>
+<i>
+<!-- ** MARIOLYS write something in here ** -->
+
+
+
+<!-- ******* End of MARIOLYS' block ******* -->
+</i> &ndash; Mariolys
+<br/>
+<br/>
+<i>
+<!-- ** GABY write something in here ** -->
+
+
+
+<!-- ******* End of GABY's block ****** -->
+</i> &ndash; Gaby
+<br/>
+<br/>
+<i>
+<!-- ** ALEXANDRA write something in here ** -->
+
+
+
+<!-- ******* End of ALEXANDRA's block ****** -->
+</i> &ndash; Alexandra
+<br/>
+<br/>
+<i>
+<!-- ** ADOLFO write something in here ** -->
+
+Mira! Un calcet&iacute;n izquierdo!
+
+<!-- ******* End of ADOLFO's block ****** -->
+</i> &ndash; Adolfo
+
+
+
+</html>
