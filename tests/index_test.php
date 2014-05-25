a1266a45236126513ff74c4839d249c20fc8498b
Adolfo Rodriguez <adolfo@merchlar.com>
2014-05-24 23:04:22 -0400

    Moved things around

---
+++ tests/index_test.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html><i>Oh yeah!</i> &ndash; The Kool Aid Man
+<br/>
+<br/>
+<i>
+<!-- ** MARIOLYS write something in here ** -->
+
+<br/><b>Hola.</b>
+<br/>
+
+<!-- ******* End of MARIOLYS' block ******* -->
+</i> &ndash; Mariolys
+<br/>
+<br/> 
+<i>
+<!-- ** GABY write something in here ** -->
+En el balcon con frio 
+
+
+
+
+<!-- ******* End of GABY's block ****** -->
+</i> &ndash; Gaby
+<br/>
+<br/>
+<i></i>
+<!-- ** ALEXANDRA write something in here ** -->
+<i>To do is to be.</i> &ndash; Socrates
+<br/>
+<i>To be or not to be.</i> &ndash; Shakespeare
+<br/>
+<i>To be is to do.</i> &ndash; Sartre
+<br/>
+<i>Dooby dooby doo.</i> &ndash; Sinatra
+<br/>
+<i>Yabba dabba doo</i> &ndash; Fred Flinstone
+<br/>
+<i>Scooby-doobee-doo</i> &ndash; Scooby Doo
+<br/>
+<br/>
+
+
+<!-- ******* End of ALEXANDRA's block ****** -->
+<i></i> &ndash; Alexandra
+<br/>
+<br/>
+<i>
+<!-- ** ADOLFO write something in here ** -->
+
+Mira!  Un calcet&iacute;n izquierdo!
+
+<!-- ******* End of ADOLFO's block ****** -->
+</i> &ndash; Adolfo
+
+
+
+</html>
