0684577f806b7ee8f1333a1fac8464d245c1c75d
mariolys07 <mariolysrivas@Mariolyss-MacBook-Air.local>
2014-05-25 00:05:41 -0400

    Added index.php for all sections

---
+++ history/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>
+			Health Passport Web App - History
+		</title>
+		<link rel="stylesheet" type="text/css" href="../css/style.css">
+	</head>
+	<body>
+	
+	</body>
+</html>
---
+++ information/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>
+			Health Passport Web App - Information
+		</title>
+		<link rel="stylesheet" type="text/css" href="../css/style.css">
+	</head>
+	<body>
+	
+	</body>
+</html>
