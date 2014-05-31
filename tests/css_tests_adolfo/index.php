e4e53a0caf4636a8b4a66b4868f0447acceec41b
Adolfo Rodriguez <adolfo@merchlar.com>
2014-05-31 16:09:38 -0400

    added css tests

---
+++ tests/css_tests_adolfo/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<style type="text/css">
+			
+		</style>
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		<script type="text/javascript">
+			$(document).ready(function(){
+				
+				
+				
+				
+			});
+			
+			
+		</script>
+	</head>
+	<body>
