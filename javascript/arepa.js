261914eb7dd86fb3bdcb09a674cffab0b77394ca
Adolfo Rodriguez <adolfo@merchlar.com>
2014-06-04 17:50:14 -0400

    Updated arepa.css,js, also tests in adolfotest.php

--- javascript/arepa.js
+++ javascript/arepa.js
@@ -4,21 +4,57 @@ var $arepa = new Object();
 //Constants
 $arepa.DETACHED_PARENT = "detachedParent";
 $arepa.PLACEHOLDER_ELEMENT = "placeholderElement";
+$arepa.ORIGINAL_BG_COLOR = "originalBackgroundColor";
+
+$arepa.MAXIMIZE_STATUS = "maximizeStatus";
+$arepa.MAXIMIZE_STATUS_STARTED = 1;
+$arepa.MAXIMIZE_STATUS_ANIMATING = 2;
+$arepa.MAXIMIZE_STATUS_FINISHING = 3;
+$arepa.MAXIMIZE_STATUS_MAXIMIZED = 4;
+
+//List of all maximized elements
+$arepa.maximizedElements = new Array();
 
 //Creates a placeholder for an HTML element
-//Detaches the original element
-//Returns true if succeeds, false otherwise
+//Detaches the original element and places it absolutely in the body
+//Returns the element's vertical offset in the document
 //Only works for position:relative/static, display:block elements
-$arepa.detachElement = function(element){
-	if (element.hasOwnProperty($arepa.DETACHED_PARENT) || element.hasOwnProperty($arepa.PLACEHOLDER_ELEMENT)){
-		//Already detached
+//with no border/padding/margin
+//Maximizes an HTML element
+$arepa.maximizeElement = function(element){
+	var offsetTop= $(element).offset().top;
+	if (element.hasOwnProperty($arepa.MAXIMIZE_STATUS)){
+		//Already detached.
 		return false;
 	}
+	if ($arepa.maximizedElements.length>0){
+		if ($(element).closest($arepa.maximizedElements[$arepa.maximizedElements.length-1]).length == 0){
+			//Not a descendant of latest maximized element
+			return false;
+		}
+	}
+	element[$arepa.MAXIMIZE_STATUS] = $arepa.MAXIMIZE_STATUS_STARTED;
+	$arepa.maximizedElements.push(element);
 	
 	//Creating DETACHED_PARENT and PLACEHOLDER_ELEMENT properties
 	element[$arepa.DETACHED_PARENT] = element.parentNode;
 	var placeholder = document.createElement("div");
 	element[$arepa.PLACEHOLDER_ELEMENT] = placeholder;
+	element[$arepa.ORIGINAL_BG_COLOR] = element.style.backgroundColor; 
+	
+	var parentElement = element;
+	var color = $(parentElement).css("background-color");
+	
+	while(color == null || color == "" || color.length==0 || (color.indexOf("rgba") === 0 && (color.match(", 0\\)$")==", 0)" || color.match(",0\\)$")==",0)")) || color=="transparent" || color=="inherit" || color=="initial"){
+		var parentElement = parentElement.parentNode;
+		if (parentElement==null || parentElement==undefined || parentElement.style==null || parentElement.style==undefined){
+			break;
+		}
+		color = $(parentElement).css("background-color");
+		//console.log("evaluating with color: "+color+" of element "+parentElement);
+	}
+	
+	//console.log("color is: "+typeof(color));
 	
 	//Obtaining height
 	var height = $(element).outerHeight();
@@ -30,21 +66,51 @@ $arepa.detachElement = function(element){
 	//Replacing with placeholder
 	$(element).replaceWith(placeholder);
 	
-	//TODO: 
+	//Making the element absolutely positioned and placing it
+	$(element).addClass("arepa-detached");
+	//Setting other css
+	$(element).css("background-color",color);
+	$(element).css("top",offsetTop);
+	$(element).css("height",height);
+	var originalOverflow = $(element).css("overflow");
+	$(element).css("overflow","hidden");
+	$(element).addClass("arepa-animate-detached");
 	
+	//Making body animated
+	$("body").addClass("arepa-animate-margin-top");
 	
-	return true;
+	//Appending element to body
+	$("body").append(element);
+	
+	element[$arepa.MAXIMIZE_STATUS] = $arepa.MAXIMIZE_STATUS_ANIMATING;
+	window.setTimeout(function(){
+		$("body").css("margin-top",-offsetTop);
+		$(element).css("top",0);
+		$(element).css("min-height",$(window).height());
+		
+		$(element).bind("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function(){
+			if (element[$arepa.MAXIMIZE_STATUS] == $arepa.MAXIMIZE_STATUS_ANIMATING){
+				element[$arepa.MAXIMIZE_STATUS] = $arepa.MAXIMIZE_STATUS_FINISHING;
+				
+				$("body").removeClass("arepa-animate-margin-top");
+				$(element).removeClass("arepa-animate-detached");
+				$(element).css("overflow",originalOverflow);
+				
+				$("body").css("margin-top",0);
+				
+				$("body").children().each(function () {
+    				if (this != element){
+    					$(this).addClass("arepa-hidden");
+    				}
+				});
+			}
+		});
+		
+		
+	},100);
 	
 }
 
-//Maximizes an HTML element
-$arepa.maximizeElement = function(element){
-	if (element.hasOwnProperty($arepa.DETACHED_PARENT)){
-		//Already maximized.
-		return $arepa;
-	}
-	element[$arepa.DETACHED_PARENT] = element.parentNode;
-	$('html, body').animate({
-        scrollTop: $(element).offset().top
-    }, 2000);
+$arepa.isElementMaximized = function(element){
+	return element.hasOwnProperty($arepa.MAXIMIZE_STATUS);
 }
--- tests/adolfotest.php
+++ tests/adolfotest.php
@@ -2,55 +2,20 @@
 <html>
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" type="text/css" href="../css/arepa.css">
 		<style type="text/css">
-			body{height:2000px;}
 			body,ul,li{margin:0px;padding:0px}
-			li{position:relative;background-color:red;color:white;height:30px}
-			li:nth-child(odd) {background: blue;}
-			a{color:inherit;position:relative;height:100%;display:block}
+			li{position:relative;display:block;background-color:red;color:white;}
+			li:nth-child(odd) {background-color: blue;}
+			a{color:inherit;}
 			.hidden{display:none;}
-			li>div{position:absolute;top:0px;left:0px;width:100%;height:100%}
-			.lidiv{position:absolute;background-color:yellow;overflow:hidden;}
-			.animated{-webkit-transition: max-height 1s linear;
-						transition: max-height 1s linear;max-height:3000}
-			.fixed-test
-			{
-				position:fixed;
-				top:0px;
-				left:0px;
-				width:100%;
-				height:300px;
-				background:orange;
-			}
 		</style>
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		<script src="../javascript/arepa.js"></script>
 		<script type="text/javascript">
 			$(document).ready(function(){
 				$("ul>li>div>a").click(function(){
-				
-					$('html, body').animate({
-						scrollTop: $(this.parentNode).offset().top
-					},200);
-				
-					return;
-				    var parent = this.parentNode;
-					//alert(parent.parentNode.tagName);
-				    if (parent.parentNode.tagName != "LI" && parent.parentNode.tagName != "li"){
-				    	//$(parent).css("height","100%");
-				    	return;
-				    }
-					var offset = $(parent).offset();
-					var height = $(parent).outerHeight();
-					parent.parentNode.removeChild(parent);
-					$(parent).css("top",offset.top);
-					$(parent).css("left",offset.left);
-					$(parent).css("width","100%");
-					$(parent).css("height",height);
-					$("body").append($(parent));
-					$(parent).addClass("lidiv");
-					$(parent).addClass("animated");
-					//$(parent).css("height","100%");
-					var myVar = setInterval(function(){$(parent).css("height","100%");clearInterval(myVar)},1000);
+					$arepa.maximizeElement(this.parentNode);
 				});
 			});
 		</script>
@@ -67,16 +32,15 @@
 			<li><div><a>hello3</a>
 				<div class="hidden">some content 3</div>
 			</div></li>
-			<li><div><a>hello3</a>
+			<li><div><a>hello4</a>
 				<div class="hidden">some content 3</div>
 			</div></li>
-			<li><div><a>hello3</a>
+			<li><div><a>hello5</a>
 				<div class="hidden">some content 3</div>
 			</div></li>
-			<li><div><a>hello3</a>
+			<li><div><a>hello6</a>
 				<div class="hidden">some content 3</div>
 			</div></li>
 		</ul>
-		<div class="fixed-test">fixed test</div>
 	</body>
 </html>
