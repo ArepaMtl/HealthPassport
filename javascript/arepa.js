92603db1544a26a3f171ce4a43a2815bd8eae7ef
Adolfo Rodriguez <adolfo@merchlar.com>
2014-06-09 10:12:19 -0400

    More arepa.js

--- javascript/arepa.js
+++ javascript/arepa.js
@@ -12,6 +12,7 @@ $arepa.ORIGINAL_OFFSET_TOP = "originalOffsetTop";
 $arepa.ORIGINAL_HEIGHT = "originalHeight";
 $arepa.ORIGINAL_SCROLL_TOP = "originalScrollTop";
 $arepa.MAXIMIZE_TIMER_COUNT = "maximizeTimerCount";
+$arepa.NON_HIDDEN_SIBLINGS = "nonHiddenSiblings";
 
 $arepa.IS_MAXIMIZED = "isMaximized";
 
@@ -43,18 +44,21 @@ $arepa.maximizeElement = function(element){
 	var scrollTop = $(document).scrollTop();
 	var offsetTop= $(element).offset().top;
 	var height = $(element).outerHeight();
+	
 	var color = $arepa.getClosestBackgroundColor(element);
+	
 	
 	
 	//Reminders
 	$arepa.setIfNull(element,$arepa.DETACHED_PARENT,element.parentNode);
 	$arepa.setIfNull(element,$arepa.PLACEHOLDER_ELEMENT,$arepa.getPlaceholderWithHeight(height));
-	$arepa.setIfNull(element,$arepa.ORIGINAL_BG_COLOR,element.style.backgroundColor);
+	$arepa.setIfNull(element,$arepa.ORIGINAL_BG_COLOR,$(element).css("background-color"));
 	$arepa.setIfNull(element,$arepa.ORIGINAL_BODY_BG_COLOR,$("body").css("background-color"));
 	$arepa.setIfNull(element,$arepa.ORIGINAL_OVERFLOW,$(element).css("overflow"));
-	$arepa.setIfNull(element,$arepa.ORIGINAL_OFFSET_TOP,height);
+	$arepa.setIfNull(element,$arepa.ORIGINAL_OFFSET_TOP,offsetTop);
 	$arepa.setIfNull(element,$arepa.ORIGINAL_SCROLL_TOP,scrollTop);
-	$arepa.setIfNull(element,$arepa.ORIGINAL_HEIGHT,offsetTop);
+	$arepa.setIfNull(element,$arepa.ORIGINAL_HEIGHT,height);
+	$arepa.setIfNull(element,$arepa.NON_HIDDEN_SIBLINGS,$arepa.nonHiddenSiblings(element));
 	
 	//Do not revert this one
 	$arepa.setIfNull(element,$arepa.MAXIMIZE_TIMER_COUNT,0);
@@ -81,7 +85,7 @@ $arepa.maximizeElement = function(element){
     	scrollTop: 0
  	}, 2000);
 	
-	element[$arepa.MAXIMIZE_TIMER_COUNT]++;
+	element[$arepa.MAXIMIZE_TIMER_COUNT] += 1;
 	
 	var currentMaximizeCounter = element[$arepa.MAXIMIZE_TIMER_COUNT];
 	
@@ -90,38 +94,44 @@ $arepa.maximizeElement = function(element){
 		if (!element[$arepa.IS_MAXIMIZED]){
 			return;
 		}
-		
+		//Stop if another animation was performed in the same context
 		if (element[$arepa.MAXIMIZE_TIMER_COUNT] != currentMaximizeCounter){
 			return;
 		}
 		
-		$("body").css("margin-top",-offsetTop);
+		$("body").css("margin-top",-element[$arepa.ORIGINAL_OFFSET_TOP]);
 		$(element).css("top",0);
 		$(element).css("min-height",$(window).height());
+		
+		element[$arepa.MAXIMIZE_TIMER_COUNT] += 1;
+		
+		currentMaximizeCounter = element[$arepa.MAXIMIZE_TIMER_COUNT];
 		
 		$(element).bind("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function(){
 			//Stop if stopped being maximized
 			if (!element[$arepa.IS_MAXIMIZED]){
 				return;
 			}
-		
-			if (!trascendentObject.animationDone){
-				trascendentObject.animationDone = true;
-				
-				$("body").removeClass("arepa-animate-margin-top");
-				$(element).removeClass("arepa-animate-detached");
-				$(element).css("overflow",element[$arepa.ORIGINAL_OVERFLOW]);
-				
-				$("body").css("margin-top",0);
-				
-				$("body").css("background-color",color);
-				
-				$("body").children().each(function () {
-    				if (this != element){
-    					$(this).addClass("arepa-hidden");
-    				}
-				});
+			//Stop if another animation was performed in the same context
+			if (element[$arepa.MAXIMIZE_TIMER_COUNT] != currentMaximizeCounter){
+				return;
 			}
+		
+			$("body").removeClass("arepa-animate-margin-top");
+			$(element).removeClass("arepa-animate-detached");
+			$(element).css("overflow",element[$arepa.ORIGINAL_OVERFLOW]);
+				
+			$("body").css("margin-top",0);
+				
+			console.log("body color: "+color);
+				
+			$("body").css("background-color",color);
+				
+			$("body").children().each(function () {
+    			if (this != element){
+    					$(this).addClass("arepa-hidden");
+    			}
+			});
 		});
 		
 	},100);
@@ -141,6 +151,28 @@ $arepa.minimizeElement = function(element){
 	}
 	element[$arepa.IS_MAXIMIZED] = false;
 	$arepa.maximizedElements.pop();
+	
+	var siblings = element[$arepa.NON_HIDDEN_SIBLINGS];
+	
+	for (var i=1;i<siblings.count;i+=1){
+		$(siblings[i]).removeClass("arepa-hidden");
+	}
+	
+	//This should happen only if it previously got to be maximized
+	//In particular the margin top should not be offset if not needed
+	//Is there a easy way to keep track at which stage it is of the previous step so that no extra steps
+	//are performed?
+	//Start by making all animation into completion blocks.
+	//Maybe make it possible to revert any sequence of completion blocks by knowing the stages
+	//Hmm interesting!
+	//Also make sure to unbind!!!!!
+	$("body").css("background-color",element[$arepa.ORIGINAL_BODY_BG_COLOR]);
+	$(element).css("overflow","hidden");
+	$(element).addClass("arepa-animate-detached");
+	$("body").addClass("arepa-animate-margin-top");
+	$("body").css("margin-top",-element[$arepa.ORIGINAL_OFFSET_TOP]);
+	
+	
 	
 	
 };
@@ -172,4 +204,28 @@ $arepa.setIfNull = function(element,key,value){
 	if (element[key]==null){
 		element[key] = value;
 	}
+};
+
+$arepa.nonHiddenSiblings = function(element){
+	var array = new Array();
+	$("body").children().each(function () {
+    	if (this != element && !$(this).hasClass("arepa-hidden")){
+    		array.push(this);
+    	}
+	});
+	return array;
+};
+
+$ArepaSimpleAnimation = function(before,wait,after){
+	this.before = before;
+	this.after = after;
+	this.wait = wait;
+	this.performWithBlocks = function(before0,before1,after0,after1){
+		if (before0 != null && !before0()){
+			return;
+		}
+	};
+	this.perform = function(){
+		this.performWithBlocks(null,null,null,null);
+	};
 };
