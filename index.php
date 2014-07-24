c066be859305e1a5b6c5839469199a9b3a73c2fd
Adolfo Rodriguez <adolfo@merchlar.com>
2014-07-23 21:04:51 -0400

    fixing sizing issues on main page

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 		</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" type="text/css" media="screen and (max-height: 600px)" href="css/style_small_height.css">
 	</head>
 	<body>
 	<div class="top-bar">
