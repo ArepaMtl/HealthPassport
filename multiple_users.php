8cd2fc2825a8cb96e5858adbc765339da5ba44b4
adolfo <correo.de.adolfo@gmail.com>
2014-12-07 16:34:46 -0500

    multiple users js functionality within multiple_users.php

--- multiple_users.php
+++ multiple_users.php
@@ -7,6 +7,29 @@
 		</title>
 		<link rel="stylesheet" type="text/css" href="css/style.css">
 		<link rel="stylesheet" type="text/css" media="screen and (max-height: 600px)" href="css/style_small_height.css">
+		<script type="text/javascript">
+			$(document).ready(function(){
+				function getUsers() {
+					var users = new Array();
+					var i = 0;
+					while (localStorage.getItem("user-"+i) != null) {
+						users.push(localStorage.getItem("user-"+i));
+						i += 1;
+					}
+					return users;
+				}
+				function addUser(name) {
+					var i = 0;
+					while (localStorage.getItem("user-"+i) != null) {
+						i += 1;
+					}
+					localStorage.setItem("user-"+i,name);
+				}
+				function removeUser(i) {
+					localStorage.removeItem("user-"+i);
+				}
+			});
+		</script>
 	</head>
 		<body>
 		<div class="top-bar">
